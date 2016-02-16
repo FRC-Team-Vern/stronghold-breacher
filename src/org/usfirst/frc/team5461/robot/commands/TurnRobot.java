cebd44e7a4b640cfdcb47cfc27502fb28e379c87
Warren <warrenlp@yahoo.com>
2016-02-15 18:16:29 -0700

    Quick fix to errors.

--- src/org/usfirst/frc/team5461/robot/commands/TurnRobot.java
+++ src/org/usfirst/frc/team5461/robot/commands/TurnRobot.java
@@ -1,4 +1,6 @@
-///package /org.usfirst.frc.team5461.robot.commands;
+
+package org.usfirst.frc.team5461.robot.commands;
+
 import org.usfirst.frc.team5461.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
@@ -30,19 +32,21 @@ public class TurnRobot extends Command {
 
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
+		boolean is_finished = false;
 		if (turnRobotDegrees < 0){
 			if (Robot.drivetrain.getImuZValue() <= turnRobotDegrees){
-				return true;
+				is_finished = true;
 			}else{
-				return false;
+				is_finished = false;
 			}
 		}else if(turnRobotDegrees > 0){
 			if (Robot.drivetrain.getImuZValue() >= turnRobotDegrees){
-				return true;
+				is_finished = true;
 			}else{
-				return false;
+				is_finished = false;
 			}
 		}
+		return is_finished;
 	}
 
 	// Called once after isFinished returns true
