1538e9ab18c34492596e2dcd446dc38a115d05c9
Lazzmozz <laztopia@gmail.com>
2016-02-20 15:00:16 -0700

    Added buttons

--- src/org/usfirst/frc/team5461/robot/OI.java
+++ src/org/usfirst/frc/team5461/robot/OI.java
@@ -1,49 +1,38 @@
 package org.usfirst.frc.team5461.robot;
 
-import org.usfirst.frc.team5461.robot.commands.TankDriveForDistance;
-
+import org.usfirst.frc.team5461.robot.commands.ChevalDeFries;
+import org.usfirst.frc.team5461.robot.commands.DriveStraight;
+import org.usfirst.frc.team5461.robot.commands.OuterWorksGroupBAndD;
+import org.usfirst.frc.team5461.robot.commands.Portcullis;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
-import edu.wpi.first.wpilibj.command.Command;
 
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-    //// CREATING BUTTONS
-    // One type of button is a joystick button which is any button on a joystick.
-    // You create one by telling it which joystick it's on and which button
-    // number it is.
-    Joystick stick = new Joystick(2);
-     
-    public OI() {
-    	Button button1 = new JoystickButton(stick,1);
-    	button1.whenPressed(new TankDriveForDistance());
-    }
+	
+	    private Joystick joy = new Joystick(0);
 
-    public Joystick getJoystick() {
-        return stick;
-    }
-    // There are a few additional built in buttons you can use. Additionally,
-    // by subclassing Button you can create custom triggers and bind those to
-    // commands the same as any other Button.
-    
-    //// TRIGGERING COMMANDS WITH BUTTONS
-    // Once you have a button, it's trivial to bind it to a button in one of
-    // three ways:
-    
-    // Start the command when the button is pressed and let it run the command
-    // until it is finished as determined by it's isFinished method.
-     
-    
-    // Run the command while the button is being held down and interrupt it once
-    // the button is released.
-    // button.whileHeld(new ExampleCommand());
-    
-    // Start the command when the button is released  and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenReleased(new ExampleCommand());
+	    public OI() {
+	    	// Put Some buttons on the SmartDashboard
+	     
+	        // Create some buttons
+	        JoystickButton x = new JoystickButton(joy, 1);
+	        JoystickButton a= new JoystickButton(joy, 2);
+	        JoystickButton b= new JoystickButton(joy, 3);
+	        JoystickButton y = new JoystickButton(joy, 8);
+	        
+
+	        // Connect the buttons to commands
+	      x.whenPressed(new ChevalDeFries());
+	      y.whenPressed(new Portcullis());
+	      b.whenPressed(new OuterWorksGroupBAndD());
+	      a.whenPressed(new DriveStraight(1));
+	    }
+	    public Joystick getJoystick() {
+	        return joy;
+	    }
 }
 
