45cacfff96a5b55a6f7b8f7280977c0ba3c4186f
bgeorgeson <bgeorgeson@live.com>
2016-03-29 19:14:33 -0600

    fixed move cannon commands

--- src/org/usfirst/frc/team5461/robot/commands/MoveCannonToMiddlePosition.java
+++ src/org/usfirst/frc/team5461/robot/commands/MoveCannonToMiddlePosition.java
@@ -35,12 +35,12 @@ public class MoveCannonToMiddlePosition extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.cannon.stopCannonMotor();
+    	Robot.cannon.stopLiftMotor();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-    	Robot.cannon.stopCannonMotor();
+    	Robot.cannon.stopLiftMotor();
     }
 }
--- src/org/usfirst/frc/team5461/robot/commands/MoveCannonToTopPosition.java
+++ src/org/usfirst/frc/team5461/robot/commands/MoveCannonToTopPosition.java
@@ -29,12 +29,12 @@ public class MoveCannonToTopPosition extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.cannon.stopCannonMotor();
+    	Robot.cannon.stopLiftMotor();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-    	Robot.cannon.stopCannonMotor();
+    	Robot.cannon.stopLiftMotor();
     }
 }
