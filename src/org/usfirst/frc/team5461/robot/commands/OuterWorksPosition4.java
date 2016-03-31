dae0d0bf681678228daa2e3ae3254d79ca6ae5c7
Guest <Guest@10.0.241.161>
2016-03-30 21:15:34 -0600

    added outerWorksPositions3 and 4 code.

--- src/org/usfirst/frc/team5461/robot/commands/OuterWorksPosition4.java
+++ src/org/usfirst/frc/team5461/robot/commands/OuterWorksPosition4.java
@@ -6,7 +6,11 @@ public class OuterWorksPosition4 extends CommandGroup {
 	
 	public OuterWorksPosition4() {
 		addSequential (new DriveStraight(200, 0.5));
-		addSequential (new TurnRobot((short)5));
+		addSequential (new TurnRobot((short)-15));
 		addSequential (new DriveStraight(200, 0.5));
+		addSequential (new MoveCannonToMiddlePosition());
+		addSequential (new HoldCannonMiddlePosition());
+		addParallel (new RunShooterMotor());
+		addParallel(new AutoFireCannon());
 	}
 }
